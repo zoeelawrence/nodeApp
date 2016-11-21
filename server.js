c95ad80c72175343a0fc9a15bace58adc5af3f3c
zoeelawrence <lawrenzoee@gmail.com>
2016-11-21 15:23:31 +0000

    hello world page

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 /**
  * Created by 1406425 on 21/11/2016.
  */
-var http = require('http')
+var http = require('http');
 var port = process.env.PORT || 1337;
 
-http.createServer(function(request, response){
+var server = http.createServer(function(request, response){
     response.writeHead(200, { 'Content-Type': 'text/plain'});
-    response.end('Hello World\n');
-}).listen(port);
+    response.write("<!DOCTYPYE 'html'>");
+    response.write("<html>");
+    response.write("<head>");
+    response.write("<title>Hello World Page</title>");
+    response.write("</head>");
+    response.write("<body>");
+    response.write("<h1>");
+    response.write("Hello World!");
+    response.write("</h1>");
+    response.write("</body>");
+    response.write("</html>");
+    response.end();
+});
+server.listen(port);
+console.log("Server is listening");

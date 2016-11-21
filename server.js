4e58e1420406830666e51c4e7b62201c3318ba22
zoeelawrence <lawrenzoee@gmail.com>
2016-11-21 15:25:43 +0000

    hello world page

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var http = require('http');
 var port = process.env.PORT || 1337;
 
 var server = http.createServer(function(request, response){
-    response.writeHead(200, { 'Content-Type': 'text/plain'});
+    response.writeHead(200, { 'Content-Type': 'text/html'});
     response.write("<!DOCTYPYE 'html'>");
     response.write("<html>");
     response.write("<head>");
